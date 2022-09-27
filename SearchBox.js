e3f7f8fe7cb4b36361c6b4cb0d63ccf7dbebed04
Tien Viet <viettienvu.97@gmail.com>
2022-09-27 18:34:30 +0700

    update

--- SearchBox.js
+++ SearchBox.js
@@ -4,11 +4,7 @@ import SearchHistory from './LocalStorage.js';
 const inputEle = document.querySelector('.search-input')
 const searchBtn = document.querySelector('.search-btn')
 const deleteBtn = document.querySelector('.delete-btn')
-const searchArray =[];
-searchArray.forEach(function(ele,index){
-    document.querySelector('.child').innerHTML = 
-    `${ele}`
-})
+console.log(SearchHistory)
 function search(){
     searchBtn.onclick = function(){
         const value = inputEle.value

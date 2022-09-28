9b1481589a972d247bd5266456bb73789c891d40
Tien Viet <viettienvu.97@gmail.com>
2022-09-28 12:38:06 +0700

    update

--- SearchBox.js
+++ SearchBox.js
@@ -3,16 +3,31 @@ import SearchHistory from './LocalStorage.js';
 
 const inputEle = document.querySelector('.search-input')
 const searchBtn = document.querySelector('.search-btn')
-const deleteBtn = document.querySelector('.delete-btn')
+const searchList = document.querySelector('.searched-history')
+
 console.log(SearchHistory)
 function search(){
+    
+    
+    const isSearchs = JSON.parse(localStorage.getItem('searching-history')) ?? {};
+    // function listSearch (element,index){
+    //     searchList.innerHTML =`
+    //     <span>${SearchHistory.get(element)}</span>
+    //     <button class="delete-search-${index}">Xoá</button>
+    //     `
+    //     const deleteBtn = document.querySelector(`.delete-search-${index}`)
+    // }
+    
     searchBtn.onclick = function(){
-        const value = inputEle.value
-        searchArray.unshift(value)
+        const time = new Date().getTime()
+        SearchHistory.set(time,inputEle.value)
+        // console.log(SearchHistory.get(isSearchs[0]))
+        // isSearchs.forEach(listSearch);
+        console.log(isSearchs)
         
     }
-    
 }
+    
 
 
-search();
+search()
